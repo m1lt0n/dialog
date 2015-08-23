bf6030aa215d75b0a859f8a711cf0eaf6e9a9cf9
Pantelis Vratsalis <pvratsalis@gmail.com>
2015-08-23 13:08:04 +0300

    PSR-2 formatting

--- src/Dialog/Output/FileOutput.php
+++ src/Dialog/Output/FileOutput.php
@@ -12,21 +12,21 @@ class FileOutput implements OutputInterface
 {
     /**
      * Holds the file resource
-     * 
+     *
      * @var resource(file)
      */
     protected $file;
 
     /**
      * Holds the path where the log files are stored
-     * 
+     *
      * @var string
      */
     protected $path = '';
 
     /**
      * Holds the DateBuilderInterface instance used to build filenames
-     * 
+     *
      * @var \Dialog\Formatter\DateTimeBuilderInterface
      */
     protected $dateBuilder;
@@ -38,9 +38,9 @@ class FileOutput implements OutputInterface
 
     /**
      * Set the path where the log files will be stored
-     * 
+     *
      * @param string $path the path
-     * @return null 
+     * @return null
      * @throws InvalidArgumentException in case the directory does not exist
      */
     public function setPath($path)
@@ -74,9 +74,9 @@ class FileOutput implements OutputInterface
     
     /**
      * Create a log file if it does not exist
-     * 
+     *
      * @return null
-     * 
+     *
      * @throws \Exception in case the file cannot be created
      */
     protected function createFileIfNotExists()
@@ -95,7 +95,7 @@ class FileOutput implements OutputInterface
 
     /**
      * Closes a file if the file name has changed
-     * 
+     *
      * @return null
      */
     protected function closeFileIfNameChanged()
@@ -113,7 +113,7 @@ class FileOutput implements OutputInterface
     
     /**
      * Gets the file path
-     * 
+     *
      * @return string the file path (path set + file name built)
      */
     protected function getFilePath()
@@ -126,7 +126,7 @@ class FileOutput implements OutputInterface
     
     /**
      * Close the file (if a file has been opened)
-     * 
+     *
      * @return null
      */
     protected function closeFile()
@@ -139,7 +139,7 @@ class FileOutput implements OutputInterface
     }
     
     /**
-     * On destruction of the FileOutput, close the logfile associated with it 
+     * On destruction of the FileOutput, close the logfile associated with it
      */
     public function __destruct()
     {
